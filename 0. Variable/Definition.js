7036355795ecb22321c3a1a1a10be533fd2ba8cc
Muhammad Fauzi Nur Aziz <axnvee18@gmail.com>
2023-06-06 13:13:40 +0700

    asd

---
+++ 0. Variable/Definition.js
@@ -0,0 +1,19 @@
+// todo - variabel (var)
+// ? = variable dinamis yang value dari variable tersebut dapat diubah/dikosongkan
+var a = 10;
+var b;
+b = 20;
+var c = a + b;
+console.log(c);
+
+// todo - variabel (let)
+// ? = sama dengan var diatas
+let i = "A";
+let j;
+j = "B";
+let k = `(${i}-${j})`;
+console.log(k);
+
+// todo - variabel (const)
+// ? = variable yang tidak dapat dikosongkan / digunakan sebagai penampung data yang sifatnya statis/tidak berubah
+const nama = "Muhammad";
