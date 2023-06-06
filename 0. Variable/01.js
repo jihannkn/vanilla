cfd62976d2972d8e16f1f385ed2d49ee0ecd2ac4
Muhammad Fauzi Nur Aziz <axnvee18@gmail.com>
2023-06-06 11:11:34 +0700

    asd

--- 0. Variable/01.js
+++ 0. Variable/01.js
@@ -1,1 +1,19 @@
-let a;
+// todo - variabel (var)
+var a = 10;
+var b;
+b = 20;
+var c = a + b;
+console.log(c);
+
+
+// todo - variabel (let)
+let i = "A";
+let j;
+j = "B";
+let k = i + " " + j;
+
+// todo - variabel (const)
+// const x = 10;
+// const y;
+// y = 20;
+// const z = x + y;
